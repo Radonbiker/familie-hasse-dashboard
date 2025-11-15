44b4609a76808b509f6bfe0ed70a17dfd7882bed
Radonbiker <hasse.matthias@gmail.com>
2025-11-15 19:33:04 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,35 @@
-const CACHE_NAME = "familie-hasse-panel-v1";
-const ASSETS = ["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
-self.addEventListener("install", e => {{
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
-}});
-self.addEventListener("activate", e => {{
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
+const CACHE_NAME = "essenplan-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./app-icon.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
   );
-}});
-self.addEventListener("fetch", e => {{
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-}});
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
