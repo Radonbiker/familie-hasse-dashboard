346bbc269899859ffcb1242a68b6da59ce7ee65a
Radonbiker <hasse.matthias@gmail.com>
2025-11-15 12:55:20 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,15 @@
-const CACHE_NAME = "familie-hasse-essenplan-v1";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.json"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
+const CACHE_NAME = "familie-hasse-panel-v1";
+const ASSETS = ["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
+self.addEventListener("install", e => {{
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+}});
+self.addEventListener("activate", e => {{
+  e.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
     )
   );
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
-  );
-});
+}});
+self.addEventListener("fetch", e => {{
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+}});
